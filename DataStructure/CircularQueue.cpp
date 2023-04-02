c23eba30b297ff41e1fd627b55e7a539ac6b67bc
Choihohee <lovelove4927@naver.com>
2023-04-02 23:15:01 +0900

    원형큐 구현 수정

--- DataStructure/CircularQueue.cpp
+++ DataStructure/CircularQueue.cpp
@@ -3,18 +3,19 @@
 #define QUEUE_SIZE 10
 
 int* queue;
-int	front;			// 전단
-int	rear;			// 후단
-int size = 1;  
+int	front=0;			// 전단
+int	rear=0;				// 후단
+int size = 1;
 
-//큐가 공백 상태인지 확인하는 연산
+
+// 큐가 공백 상태인지 확인하는 연산
 int isEmpty() {
 	if (front == rear) 
 		return 1;
 	else 
 		return 0;
 }
-  
+
 // 큐가 포화 상태인지 확인하는 연산
 int isFull() {
 	if (front == (rear + 1) % QUEUE_SIZE) 
@@ -24,7 +25,7 @@ int isFull() {
 }
 
 int queueSize() {
-	return(rear - front + QUEUE_SIZE) % QUEUE_SIZE;
+		return(rear - front + QUEUE_SIZE) % QUEUE_SIZE;
 }
 
 // 스택의 top에 원소를 삽입하는 연산
@@ -45,8 +46,7 @@ int dequeue() {
 		printf("\n\n Queue is Empty!!\n");
 		return 0;
 	}
-	else
-	{
+	else{
 		front = (front + 1) % QUEUE_SIZE;
 		return queue[front];
 	}
@@ -56,6 +56,7 @@ int dequeue() {
 // 큐의 원소를 출력하는 연산
 void printQueue() {
 	int i, maxi = rear;
+
 	if (front >= rear) 
 		maxi += QUEUE_SIZE;
 	printf("Queue size is [%2d]= ", queueSize());
@@ -69,11 +70,14 @@ void main(void) {
 	int i;
 	queue = (int*)malloc(QUEUE_SIZE * sizeof(int));
 
-	for (i = 0; i < 9; i++) enqueue(i + 1);
+	for (i = 0; i < 9; i++) 
+		enqueue(i + 1);
 	printQueue();
-	for (i = 0; i < 7; i++) dequeue();
+	for (i = 0; i < 7; i++) 
+		dequeue();
 	printQueue();
-	for (i = 10; i < 19; i++) enqueue(i + 1);
+	for (i = 10; i < 19; i++) 
+		enqueue(i + 1);
 	printQueue();
 
 	free(queue);
